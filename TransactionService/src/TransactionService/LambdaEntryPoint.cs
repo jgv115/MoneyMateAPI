4d16d54eb755e66fa95f288a7a7db8086452b995
Benjamin Ong <jgv115@gmail.com>
2021-03-07 17:04:19 +1100

    Fixed unit tests and added them to workflow

--- TransactionService/src/TransactionService/LambdaEntryPoint.cs
+++ TransactionService/src/TransactionService/LambdaEntryPoint.cs
@@ -24,6 +24,7 @@ namespace TransactionService
         {
             builder
                 .UseStartup<Startup>();
+
         }
 
         /// <summary>
@@ -36,5 +37,7 @@ namespace TransactionService
         protected override void Init(IHostBuilder builder)
         {
         }
+        
+        
     }
 }
--- TransactionService/test/TransactionService.Tests/ValuesControllerTests.cs
+++ TransactionService/test/TransactionService.Tests/ValuesControllerTests.cs
@@ -21,12 +21,28 @@ namespace TransactionService.Tests
             var lambdaFunction = new LambdaEntryPoint();
 
             var requestStr = File.ReadAllText("./SampleRequests/ValuesController-Get.json");
-            var request = JsonConvert.DeserializeObject<APIGatewayProxyRequest>(requestStr);
+            // var request = JsonConvert.DeserializeObject<APIGatewayHttpApiV2ProxyRequest>(requestStr);
+            var request = new APIGatewayHttpApiV2ProxyRequest()
+            {
+                Version = "2.0",
+                RawPath = "api/values",
+                RequestContext = new APIGatewayHttpApiV2ProxyRequest.ProxyRequestContext()
+                {
+                    AccountId = "123456789012",
+                    ApiId = "api-id",
+                    Http = new APIGatewayHttpApiV2ProxyRequest.HttpDescription()
+                    {
+                        Method = "GET",
+                        Path = "api/values",
+                        Protocol = "HTTP/1.1"
+                    }
+                }
+            };
             var context = new TestLambdaContext();
             var response = await lambdaFunction.FunctionHandlerAsync(request, context);
-
             Assert.Equal(200, response.StatusCode);
             Assert.Equal("[\"value1\",\"value2\"]", response.Body);
+            
             // Assert.True(response.MultiValueHeaders.ContainsKey("Content-Type"));
             // Assert.Equal("application/json; charset=utf-8", response.MultiValueHeaders["Content-Type"][0]);
         }
