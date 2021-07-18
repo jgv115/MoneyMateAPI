4eaac4b88e27d1d990a1c34f7d738fb47deceda0
Benjamin Ong <jgv115@gmail.com>
2021-07-18 23:28:51 +1000

    Add iam user for auth0 hooks

---
+++ utilities/categoryInitialiser/infra/iam_users.tf
@@ -0,0 +1,37 @@
+resource "aws_iam_user" auth0_hooks {
+  name = "auth0_hooks_user_${terraform.workspace}"
+
+  tags = local.tags
+}
+
+resource "aws_iam_access_key" auth0_hooks {
+  user = aws_iam_user.auth0_hooks
+  tags = local.tags
+}
+
+data aws_iam_policy_document auth0_hooks {
+  statement {
+    effect = "Allow"
+    actions = [
+      "lambda:InvokeFunction"
+    ]
+    resources = [
+      aws_lambda_function.category_initialiser_lambda.arn
+    ]
+  }
+}
+
+resource aws_iam_user_policy auth0_hooks {
+  policy = data.aws_iam_policy_document.auth0_hooks.json
+  user = aws_iam_user.auth0_hooks.name
+  tags = local.tags
+}
+
+
+
+resource "aws_ssm_parameter" auth0_hooks_secret {
+  name = "auth0_hooks_user_${terraform.workspace}"
+  type = "SecureString"
+  value = aws_iam_access_key.auth0_hooks.encrypted_secret
+  tags = local.tags
+}
