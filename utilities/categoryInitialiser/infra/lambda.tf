dfe9e3eef583bb265b11a89435b208e4a86e36b1
Benjamin Ong <jgv115@gmail.com>
2021-07-18 22:45:56 +1000

    Add ECR lifecycle policy to category initialiser repo

--- utilities/categoryInitialiser/infra/lambda.tf
+++ utilities/categoryInitialiser/infra/lambda.tf
@@ -2,6 +2,28 @@ data "aws_ecr_repository" "category_initialiser_image_repository" {
     name = "moneymate_category_initialiser"
 }
 
+resource "aws_ecr_repository_policy" ecr_policy {
+    repository = data.aws_ecr_repository.category_initialiser_image_repository.name
+    policy = <<EOF
+{
+  "rules": [
+    {
+      "rulePriority": 1,
+      "description": "Keep only 2 images in ECR",
+      "selection": {
+        "tagStatus": "any",
+        "countType": "imageCountMoreThan",
+        "countNumber": 2
+      },
+      "action": {
+        "type": "expire"
+      }
+    }
+  ]
+}
+EOF
+}
+
 resource "aws_lambda_function" category_initialiser_lambda {
     function_name = local.category_initialiser_lambda_name
     image_uri = "${data.aws_ecr_repository.category_initialiser_image_repository.repository_url}:${var.CATEGORY_INITIALISER_LAMBDA_IMAGE_TAG}"
