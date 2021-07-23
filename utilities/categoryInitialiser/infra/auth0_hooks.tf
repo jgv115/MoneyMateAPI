ae13208045befb54e1645c0736bd9a51e0a5f1c1
Benjamin Ong <jgv115@gmail.com>
2021-07-23 21:01:39 +1000

    Update auth0 hook

--- utilities/categoryInitialiser/infra/auth0_hooks.tf
+++ utilities/categoryInitialiser/infra/auth0_hooks.tf
@@ -1,13 +1,25 @@
 resource "auth0_hook" post_user_registration {
   name = "Post User Registration - Category Initialiser"
   script = <<EOF
-function (user, context, callback) {
-  console.log(context)
-  console.log(${terraform.workspace})
-  console.log(context.webtask.secrets.test_secret)
-  
+module.exports = async (user, context, callback) => {
+
+  console.log("${terraform.workspace}")
+  var AWS = require("aws-sdk")
+
+  AWS.config.update({
+      accessKeyId: context.webtask.secrets.aws_client_id, secretAccessKey: context.webtask.secrets.aws_client_secret, region: "ap-southeast-2"
+  })
+
+  const result = await (new AWS.Lambda().invoke({
+      FunctionName: "category_initialiser_lambda_test",
+      // Payload: "test"
+  }).promise());
+
+  console.log(result)
+
   callback(null, { user });
 }
+
 EOF
   trigger_id = "post-user-registration"
   enabled = true
@@ -16,3 +28,4 @@ EOF
     aws-sdk = "2.950.0"
   }
 }
+
