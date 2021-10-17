c60250451fe5ffcef6476fefd37c70660c0a9c95
Benjamin Ong <jgv115@gmail.com>
2021-10-17 22:40:05 +1100

    Add extra valid datetime formats to puttransactiondto

--- TransactionService/src/TransactionService/Validators/PutTransactionDtoValidator.cs
+++ TransactionService/src/TransactionService/Validators/PutTransactionDtoValidator.cs
@@ -15,9 +15,15 @@ namespace TransactionService.Validators
                 var validFormats = new[]
                 {
                     "yyyy-MM-ddThh:mm:ssZ",
+                    "yyyy-MM-ddThh:mm:ss.FZ",
+                    "yyyy-MM-ddThh:mm:ss.FFZ",
+                    "yyyy-MM-ddThh:mm:ss.FFFZ",
                     "yyyy-MM-ddThh:mm:ssz",
+                    "yyyy-MM-ddThh:mm:ss.FFFz",
                     "yyyy-MM-ddThh:mm:sszz",
-                    "yyyy-MM-ddThh:mm:sszzz"
+                    "yyyy-MM-ddThh:mm:ss.FFFzz",
+                    "yyyy-MM-ddThh:mm:sszzz",
+                    "yyyy-MM-ddThh:mm:ssFFFzzz"
                 };
 
                 DateTime dt;
