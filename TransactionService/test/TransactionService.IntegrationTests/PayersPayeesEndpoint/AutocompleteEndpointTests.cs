25d3c8a80888be5e674d1cd0eeeb261e487c3b65
Benjamin Ong <jgv115@gmail.com>
2022-03-15 18:22:42 +1100

    Add more integration tests around autocomplete endpoint

--- TransactionService/test/TransactionService.IntegrationTests/PayersPayeesEndpoint/AutocompleteEndpointTests.cs
+++ TransactionService/test/TransactionService.IntegrationTests/PayersPayeesEndpoint/AutocompleteEndpointTests.cs
@@ -218,4 +218,84 @@ public class AutocompleteEndpointTests : IClassFixture<MoneyMateApiWebApplicatio
 
         Assert.Equal(new List<PayerPayeeViewModel> {expectedPayer2}, returnedPayees);
     }
+    
+    [Theory]
+    [InlineData("multiword")]
+    [InlineData("multiword pa")]
+    [InlineData("Multiword Pa")]
+    [InlineData("multiword Payer")]
+
+    public async Task
+        GivenValidRequestWithMultipleSearchWords_WhenGetAutocompletePayersEndpointCalled_ThenCorrectPayersReturned(string searchQuery)
+    {
+        var payee1 = new PayerPayee
+        {
+            UserId = UserId,
+            PayerPayeeId = "payee#9540cf4a-f21b-4cac-9e8b-168d12dcecfb",
+            PayerPayeeName = "payer1",
+            ExternalId = Guid.NewGuid().ToString()
+        };
+        var payer2 = new PayerPayee
+        {
+            UserId = UserId,
+            PayerPayeeId = "payer#9540cf4a-f21b-4cac-9e8b-168d12dcecfc",
+            PayerPayeeName = "Multiword Payer",
+            ExternalId = Guid.NewGuid().ToString()
+        };
+        var expectedPayer2 = new PayerPayeeViewModel
+        {
+            PayerPayeeId = Guid.Parse("9540cf4a-f21b-4cac-9e8b-168d12dcecfc"),
+            PayerPayeeName = "Multiword Payer",
+            ExternalId = payer2.ExternalId
+        };
+        
+        var payer3 = new PayerPayee
+        {
+            UserId = UserId,
+            PayerPayeeId = "payer#9540cf4a-f21b-4cac-9e8b-168d12dcec12",
+            PayerPayeeName = "Multiword Payer 123",
+            ExternalId = Guid.NewGuid().ToString()
+        };
+        var expectedPayer3 = new PayerPayeeViewModel
+        {
+            PayerPayeeId = Guid.Parse("9540cf4a-f21b-4cac-9e8b-168d12dcec12"),
+            PayerPayeeName = "Multiword Payer 123",
+            ExternalId = payer3.ExternalId
+        };
+        
+        var initialData = new List<PayerPayee>
+        {
+            new()
+            {
+                UserId = UserId,
+                PayerPayeeId = "payer#9540cf4a-f21b-4cac-9e8b-168d12dcecfd",
+                PayerPayeeName = "payer1",
+                ExternalId = Guid.NewGuid().ToString()
+            },
+            new()
+            {
+                UserId = UserId,
+                PayerPayeeId = "payer#9540cf4a-f21b-4cac-9e8b-168d12dcecfe",
+                PayerPayeeName = "payer2",
+                ExternalId = Guid.NewGuid().ToString()
+            },
+            payee1,
+            payer2,
+            payer3
+        };
+
+        await _dynamoDbHelper.WriteIntoTable(initialData);
+
+        var response = await _httpClient.GetAsync($"/api/payerspayees/payers/autocomplete?name={searchQuery}");
+        response.EnsureSuccessStatusCode();
+
+        var returnedString = await response.Content.ReadAsStringAsync();
+        var returnedPayees = JsonSerializer.Deserialize<List<PayerPayeeViewModel>>(returnedString,
+            new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true
+            });
+
+        Assert.Equal(new List<PayerPayeeViewModel> {expectedPayer3, expectedPayer2}, returnedPayees);
+    }
 }
