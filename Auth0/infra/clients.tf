241a835d69d30f27226132fee1c07ba0f0e3e0a2
Benjamin Ong <jgv115@gmail.com>
2023-01-06 20:27:06 +1100

    Add MoneyMate Web auth0 client

--- Auth0/infra/clients.tf
+++ Auth0/infra/clients.tf
@@ -26,6 +26,36 @@ resource "auth0_client" moneymate_app {
   }
 }
 
+resource "auth0_client" moneymate_web {
+  name = "MoneyMate Web"
+  app_type = "regular_web"
+  callbacks = ["http://localhost:3000/callback"]
+  allowed_logout_urls                 = ["http://localhost:3000/callback"]
+  web_origins                         = ["http://localhost:3000/callback"]
+  cross_origin_auth = false
+  custom_login_page_on = true
+  grant_types = ["authorization_code", "implicit", "refresh_token"]
+  is_first_party = true
+  is_token_endpoint_ip_header_trusted = false
+  oidc_conformant = true
+  sso_disabled = false
+  token_endpoint_auth_method = "none"
+  jwt_configuration {
+    alg = "RS256"
+    lifetime_in_seconds = 36000
+    secret_encoded = false
+  }
+  refresh_token {
+    rotation_type = "rotating"
+    expiration_type = "expiring"
+    leeway = 0
+    token_lifetime = 2592000
+    infinite_idle_token_lifetime = true
+    infinite_token_lifetime      = false
+    idle_token_lifetime          = 1296000
+  }
+}
+
 resource "auth0_client" debug_app {
   name = "Debug App"
   app_type = "regular_web"
