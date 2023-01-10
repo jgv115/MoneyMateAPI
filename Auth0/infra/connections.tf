f522c2987730ce00d492d4489957910c54d9918a
Benjamin Ong <jgv115@gmail.com>
2023-01-10 23:50:24 +1100

    Add auth0 connections to moneymate web

--- Auth0/infra/connections.tf
+++ Auth0/infra/connections.tf
@@ -2,7 +2,7 @@ resource "auth0_connection" username_password {
   name = "Username-Password-Authentication"
   strategy = "auth0"
   is_domain_connection = false
-  enabled_clients = [auth0_client.moneymate_app.id, auth0_client.debug_app.id, var.auth0_management_api_client_id]
+  enabled_clients = [auth0_client.moneymate_app.id, auth0_client.moneymate_web, auth0_client.debug_app.id, var.auth0_management_api_client_id]
 }
 
 resource "auth0_connection" google {
@@ -12,5 +12,5 @@ resource "auth0_connection" google {
   }
   strategy = "google-oauth2"
   is_domain_connection = false
-  enabled_clients = [auth0_client.moneymate_app.id]
+  enabled_clients = [auth0_client.moneymate_app.id, auth0_client.moneymate_web]
 }
