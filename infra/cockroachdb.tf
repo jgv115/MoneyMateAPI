6653b38df7e36910d47563e6cb253c5798ff847b
Benjamin Ong <jgv115@gmail.com>
2023-07-23 21:17:09 +1000

    Remove cockroach db stuff

--- infra/cockroachdb.tf
+++ infra/cockroachdb.tf
@@ -1,15 +1,15 @@
-resource "cockroach_cluster" "moneymate_cluster" {
-  name           = var.cockroach_db_cluster_name
-  cloud_provider = "AWS"
-  serverless = {
-    spend_limit = 0
-  }
-  regions = [{
-    name = "ap-southeast-1"
-  }]
-}
-
-resource cockroach_database moneymate_database {
-  cluster_id = cockroach_cluster.moneymate_cluster.id
-  name = "moneymate_db_${local.tags.environment}"
-}
+#resource "cockroach_cluster" "moneymate_cluster" {
+#  name           = var.cockroach_db_cluster_name
+#  cloud_provider = "AWS"
+#  serverless = {
+#    spend_limit = 0
+#  }
+#  regions = [{
+#    name = "ap-southeast-1"
+#  }]
+#}
+#
+#resource cockroach_database moneymate_database {
+#  cluster_id = cockroach_cluster.moneymate_cluster.id
+#  name = "moneymate_db_${local.tags.environment}"
+#}
