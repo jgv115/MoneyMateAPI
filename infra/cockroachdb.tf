ed7bfa76db1511eb173c34ca14b54c2a01a3aa6e
Benjamin Ong <jgv115@gmail.com>
2023-07-23 20:51:04 +1000

    Add moneymate cluster to terraform

--- infra/cockroachdb.tf
+++ infra/cockroachdb.tf
@@ -1,4 +1,13 @@
+resource "cockroach_cluster" "moneymate_cluster" {
+  name           = var.cockroach_db_cluster_name
+  cloud_provider = "AWS"
+  serverless = {
+    spend_limit = 0
+  }
+  regions = ["ap-southeast-1"]
+}
+
 resource cockroach_database moneymate_database {
-  cluster_id = var.cockroach_db_cluster_name
+  cluster_id = cockroach_cluster.moneymate_cluster.id
   name = "moneymate_db_${local.tags.environment}"
 }
